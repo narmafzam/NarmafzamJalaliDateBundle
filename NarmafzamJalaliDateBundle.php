2d7375f4d66af2d990d1b2d8a2d36d4fbdc16ef9
Peyman Mohamadpour <peyman.mohamadpour@yahoo.com>
2018-08-27 10:55:39 +0430

    Rellocate namespaced classes

---
+++ NarmafzamJalaliDateBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of cardioclinic
+ * Copyrighted by Narmafzam (Farzam Webnegar Sivan Co.), info@narmafzam.com
+ * Created by peyman
+ * Date: 08/27/2018
+ */
+
+namespace Narmafzam\JalaliDateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NarmafzamJalaliDateBundle extends Bundle
+{
+
+}
