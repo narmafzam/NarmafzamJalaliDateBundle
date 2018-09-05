cd0dbe3247d4eda16af0b852e94b7139175b2b1c
Peyman Mohamadpour <peyman.mohamadpour@yahoo.com>
2018-09-05 17:36:09 +0430

    Add date model transformer

--- Form/Type/NarmafzamJalaliGregorianDateType.php
+++ Form/Type/NarmafzamJalaliGregorianDateType.php
@@ -8,6 +8,7 @@
 
 namespace Narmafzam\JalaliDateBundle\Form\Type;
 
+use Narmafzam\JalaliDateBundle\Form\DataTransformer\NarmafzamDateTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,15 +20,55 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class NarmafzamJalaliGregorianDateType extends AbstractType
 {
     /**
+     * @var string
+     */
+    protected $locale;
+
+    /**
+     * NarmafzamJalaliDateType constructor.
+     *
+     * @param DateConverter $dateConverter
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->locale = $requestStack->getCurrentRequest()->getLocale() == 'fa' ? 'fa' : 'en';
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $transformer = new NarmafzamDateTransformer($this->dateConverter, $options['serverFormat'], $options['locale']);
+        $builder->addModelTransformer($transformer);
+    }
+
+    /**
      * @param OptionsResolver $resolver
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
+            'serverFormat' => 'yyyy/MM/dd',
+            'locale' => $this->locale,
             'parent_attr' => array(
                 'class'   => 'col-sm-6'
             )
         ));
+
+        $resolver->setAllowedTypes('serverFormat', ['string', 'null']);
+        $resolver->setAllowedTypes('locale', ['string', 'null']);
+        $resolver->setAllowedValues('locale', ['fa', 'en', null]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function setDefaultOptions(OptionsResolver $resolver)
+    {
+        $this->configureOptions($resolver);
     }
 
     /**
@@ -44,5 +85,13 @@ class NarmafzamJalaliGregorianDateType extends AbstractType
     public function getBlockPrefix()
     {
         return 'narmafzam_jalali_gregorian_date';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
     }
 }
